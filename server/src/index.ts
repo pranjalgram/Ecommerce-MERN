713376f606fac298deb4ee40a2eb2c70e6a19004
Pranjal Singh <mailingpranjal@gmail.com>
2024-07-11 05:14:50 +0530

    User authentication

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import { userRouter } from './routes/user';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
+
+mongoose.connect("mongodb+srv://pranjalgram:ecommercepassword@ecommerce.y1c2eqr.mongodb.net/ecommerce");
+
+app.listen(3001, () => console.log("SERVER STARTED"));
